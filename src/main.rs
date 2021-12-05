2f27b145d310d5cf2442f89f5ead3a7ce422dbbe
Rael Calitro <rael06@hotmail.fr>
2021-12-05 12:37:45 +0100

    pointers step

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,87 @@
 fn main() {
+    // let my_var1 = 0xFE;
+    // let my_var2 = 0b10011;
+    // let mut my_var: u32 = 13.25f32 as u32;
+    // const MY_CONSTANT: f32 = 3.14;
+    // println!("my_var = {} - MY_CONSTANT = {}", my_var, MY_CONSTANT);
+    // my_var = 15;
+    // println!("my_var = {}", my_var);
+    //
+    // let my_text = "My text";
+    // println!("my_text = {}", my_text);
+    //
+    // let mut my_text_string: String = String::from("My text string");
+    // println!("my_text_string = {}", my_text_string);
+    //
+    // my_text_string.push_str(" longer");
+    // println!("my_text_string = {}", my_text_string);
+    //
+    // let my_var_string = my_var.to_string();
+    // println!("my_var_string = {}", my_var_string);
+
+    // let a: u32 = 89;
+    // let mut b = a;
+    // println!("a = {} - b = {}", a, b);
+    // b = 13;
+    // println!("a = {} - b = {}", a, b);
+    //
+    // let c = "Hello".to_owned();
+    // let mut d = c.clone();
+    // d.push_str(" world");
+    // println!("c = {} - d = {}", c, d);
+    // println!("d = {}", d);
+
+    // let a = "Hello".to_owned();
+    // {
+    //     let b = a.clone();
+    //     println!("b = {}", b);
+    // }
+
+    // let a = 18;
+    // let result =
+    //     if a >= 18 { "maj" }
+    // else { "min" };
+    //
+    // println!("a = {}", a);
+    // println!("result = {}", result);
+
+    // let a = 25;
+    //
+    // match a {
+    //     0 => println!("zero"),
+    //     _ if a < 0 => println!("error"),
+    //     1..=17 => println!("min"),
+    //     19 | 24 | 32 => println!("Ok"),
+    //     n if n % 5 == 0 => println!("multiple of 5"),
+    //     _ => println!("maj"),
+    // }
+
+    // let (a, (_, mut b)) = (56, (30, 125));
+    // println!("a = {} - b = {}", a, b);
+    // b = 15;
+    // println!("a = {} - b = {}", a, b);
+
+    // let a = ("div", 50, 3);
+    //
+    // let b = match a {
+    //     (operator, n1, n2) if operator == "div" && n2 !=0 => (operator, n1 as f32 / n2 as f32),
+    //     (operator, n1, n2) if operator == "add" => (operator, (n1 + n2) as f32),
+    //     (operator, n1, _) if operator == "sqr" => (operator, (n1 * n1) as f32),
+    //     _ => ("error", 0f32),
+    // };
+    //
+    // println!("operator = {} - result = {}", b.0, b.1);
+
+    // let mut a = "Hello".to_owned();
+    // {
+    //     let b = &a;
+    //     println!("b = {}", b);
+    //     let c = &mut a;
+    //     c.push_str(" world");
+    //     println!("c = {}", c);
+    // }
+    // let d = &a;
+    // println!("a = {}", a);
+
     println!("Hello, world!");
 }
